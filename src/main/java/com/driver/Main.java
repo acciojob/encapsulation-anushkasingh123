2b804e4679164faf5d31bffbe5f43cd56f74c9b6
Anushka <anushkasingh590326@gmail.com>
2023-01-17 16:43:43 +0530

    encapsulation first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,18 @@
 package com.driver;
 
 public class Main {
+
+    public static void main(String args[])
+    {
+        RWOnly obj=new RWOnly();
+//        obj.name="hello";
+//        System.out.println(obj.name);
+//        java: name has private access in com.driver.RWOnly
+        obj.setName("new me");
+        obj.getName();
+        System.out.println(obj.getName());
+
+
+    }
   
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class RWOnly {
-    
+
+
+        private String name="java";
+
+        public String getName() {
+                return name;
+        }
+
+        public void setName(String name) {
+                this.name = name;
+        }
 }
